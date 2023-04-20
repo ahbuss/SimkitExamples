3adc3db60fabf065c78b0a8bf42103ced112ba55
Arnie Buss <abuss@nps.edu>
2023-04-19 18:40:07 -0700

    TwoCustomerTypesServer

--- src/simkit/test/TestTransferLineWithBlocking.java
+++ src/simkit/test/TestTransferLineWithBlocking.java
@@ -17,7 +17,7 @@ public class TestTransferLineWithBlocking {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        RandomVariate interarrivalTimeGenerator = RandomVariateFactory.getInstance("Uniform", 0.0, 2.0);
+        RandomVariate interarrivalTimeGenerator = RandomVariateFactory.getInstance("Uniform", 0.0, 0.5);
         int value = 0;
         IntArrivalProcess intArrivalProcess = new IntArrivalProcess(interarrivalTimeGenerator, value);
         System.out.println(intArrivalProcess);
@@ -26,9 +26,9 @@ public class TestTransferLineWithBlocking {
         int[] totalNumberMachines = {2, 1, 1, 1};
         RandomVariate[] processingTimeGenerator = new RandomVariate[]{
             RandomVariateFactory.getInstance("Uniform", 0.0, 1.0),
-            RandomVariateFactory.getInstance("Uniform", 2.5, 4.5),
-            RandomVariateFactory.getInstance("Uniform", 1.7, 3.7),
-            RandomVariateFactory.getInstance("Uniform", 2.8, 4.8),};
+            RandomVariateFactory.getInstance("Uniform", 0.0, 1.0),
+            RandomVariateFactory.getInstance("Uniform", 0.0, 1.0),
+            RandomVariateFactory.getInstance("Uniform", 10.0, 20.0),};
         int[] queueCapacity = {Integer.MAX_VALUE, 1, 1, 1};
         
         TransferLineWithBlocking transferLineWithBlocking = new TransferLineWithBlocking(numberMachines, totalNumberMachines, processingTimeGenerator, queueCapacity);
@@ -39,8 +39,8 @@ public class TestTransferLineWithBlocking {
         SimplePropertyDumper simplePropertyDumper = new SimplePropertyDumper();
         transferLineWithBlocking.addPropertyChangeListener(simplePropertyDumper);
 
-//        Schedule.stopOnEvent(20, "Unblock", Integer.class);
-        Schedule.stopAtTime(100.0);
+        Schedule.stopOnEvent(20, "Unblock", Integer.class);
+//        Schedule.stopAtTime(100.0);
         Schedule.setVerbose(true);
         
         Schedule.reset();
